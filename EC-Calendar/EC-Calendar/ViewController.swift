bbdcf72706d70e3942a82fc4a3a1b116a5e46b79
jojohn123 <44539812+jojohn123@users.noreply.github.com>
2019-02-22 14:22:42 +0800

    Initialisation

---
+++ EC-Calendar/EC-Calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EC-Calendar
+//
+//  Created by LAM Chun Kit on 22/2/2019.
+//  Copyright © 2019 Group8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
